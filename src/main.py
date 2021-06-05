5d61b46b5d5cbef3ab10d51812e852ce074008dd
entrity0305 <79962463+entrity0305@users.noreply.github.com>
2021-06-05 11:05:05 +0900

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -4,11 +4,18 @@ from language.parse import Parser
 
 if __name__ == '__main__':
     code = '''
-변수 는 ["EOS 는 1", 3.14, "안녕, 세상!"] 이고
+변수 는 ["","EOS 는 1", 3.14, "안녕, 세상!", "EOS"] 더하기 3 이고
 나 는 3.141575 더하기 변수 임
+
+
+
+출력하는변수 은 출력("안녕, 세상!") 임
 '''
 
     hunminLexer = Lexer(' '.join(code.split('\n')))
     hunminParser = Parser()
     print(hunminLexer.lex())
     print(hunminParser.parse(hunminLexer.lex()))
+
+
+    a = Lexer()
